33c4b7304c9dec046f8816cf3eb1e27fcc04fcf5
AzzeddineGAZOU <azdine.gazou@gmail.com>
2020-06-27 14:52:25 +0200

    add missing index.ts

---
+++ index.ts
@@ -0,0 +1,63 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import session from 'express-session';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { Address } from './src/entity/Address';
+import { Ingredient } from './src/entity/Ingredient';
+import { IngredientMold } from './src/entity/IngredientMold';
+import { Mold } from './src/entity/Mold';
+import { PartnerRecovery } from './src/entity/PartnerRecovery';
+import { PartnershipRequest } from './src/entity/PartnershipRequest';
+import { Recovery } from './src/entity/Recovery';
+import { Role } from './src/entity/Role';
+import { User } from './src/entity/User';
+import { router } from './src/routes/routes';
+import { Reward } from './src/entity/Reward';
+
+const app = express();
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+}));
+app.use(cors({ origin : true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+
+app.use(router);
+
+// Connexion à la base
+createConnection({
+    type : 'mysql',
+    host : 'localhost', 
+    port : 3306,
+    username : 'root',
+    password : '',
+    database : 'seb_test',
+    synchronize : true,
+    logging : false,
+    entities : [
+        Address,
+        Role,
+        User,
+        PartnershipRequest,
+        PartnerRecovery,
+        Mold,
+        IngredientMold,
+        Ingredient,
+        Recovery,
+        Reward
+    ]
+}).then(async connection => {
+    console.log('Connected to DB');
+    // Connexion au serveur 
+    app.listen(3000, () => {
+        console.log('Server listening on port 3000');
+    });
+}).catch(error => console.log('TypeORM connection error: ', error));
+
+export default app;
